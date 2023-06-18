e5ea6780347dcfef1e65443e3b5d10a2f953064e
animireddy-san <animireddysantosh@gmail.com>
2023-06-18 13:32:59 +0530

    first commit

---
+++ src/main/java/com/santechie/os/api/SpringbootandKubApplication.java
@@ -0,0 +1,13 @@
+package com.santechie.os.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootandKubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootandKubApplication.class, args);
+	}
+
+}
